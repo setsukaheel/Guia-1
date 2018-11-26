485b9c8b9a55cbcd863b67a496df4ee3cc4268cf
Maria <44624232+setsukaheel@users.noreply.github.com>
2018-11-25 23:22:24 -0300

    Ejercicio N° 18
    Cálculo de los múltiplos de 2 y 3 entre 1 y 100

---
+++ ejercicio18[1].java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ejercicio18 {
+
+	public static void main(String[] args) {
+		//Cuenta de numeros multiplos de 2 o 3 que hay entre 1 y 100.
+		
+		
+	
+		
+		System.out.println("Numeros multiplos de 2: ");
+		
+		for(int i=0 ; i<101; i++){
+		  if (i % 2==0) {
+			  System.out.print(i + " - ");
+		  }
+		}
+		System.out.println("");
+		System.out.println("Numeros multiplos de 3: ");
+		  for(int i=0; i<101; i++) {
+			  if (i % 3==0) {
+				  System.out.print(i + " - ");
+		     }
+		  }
+		}
+	}
+			
+			
+		
+
